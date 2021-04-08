2c211cab28f52b4df2f8979351d01200c22cc3cd
joneirikboska <joneirikboska@gmail.com>
2021-04-08 11:53:51 +0200

    Create video connection with socket.on functions on client and server side.

--- src/chat-server.ts
+++ src/chat-server.ts
@@ -1,6 +1,7 @@
 import * as express from 'express'
 import { createServer, Server } from 'http'
 import * as socketIo from 'socket.io'
+import * as path from 'path'
 
 export class ChatServer {
   public static readonly PORT: number = 5000
@@ -8,6 +9,7 @@ export class ChatServer {
   private port: string | number
   private server: Server
   private io: socketIo.Server
+  private socketsArray = []
 
   constructor() {
     this.createApp()
@@ -23,7 +25,7 @@ export class ChatServer {
 
   private createApp(): void {
     this.app = express()
-    this.app.use(express.static('public'))
+    this.app.use('/', express.static(path.join(__dirname, 'public')))
   }
 
   private config(): void {
@@ -34,9 +36,29 @@ export class ChatServer {
     this.server.listen(this.port, () => {
       console.log('Running server on port %s', this.port)
     })
+
     this.io.on('connection', socket => {
       socket.broadcast.emit('add-users', {
         users: [socket.id],
+      })
+
+      socket.on('disconnect', () => {
+        this.socketsArray.splice(this.socketsArray.indexOf(socket.id), 1)
+        this.io.emit('remove-user', socket.id)
+      })
+
+      socket.on('make-offer', function (data) {
+        socket.to(data.to).emit('offer-made', {
+          offer: data.offer,
+          socket: socket.id,
+        })
+      })
+
+      socket.on('make-answer', function (data) {
+        socket.to(data.to).emit('answer-made', {
+          socket: socket.id,
+          answer: data.answer,
+        })
       })
     })
   }
